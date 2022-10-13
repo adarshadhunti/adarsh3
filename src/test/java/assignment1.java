576db75913853ff89d6231e8246fced7d7ec6e88
adarshadhunti <adarshadhunti@gmail.com>
2022-10-13 09:44:12 +0530

    Create assignment1.java
    qa

---
+++ src/test/java/assignment1.java
@@ -0,0 +1,14 @@
+public class assignment1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+       // Write a program to swap two number. For example a=10 and b=20 output should be a=20 and b=10
+int a,b,c;
+a=20;
+b=10;
+c=a;
+a=b;
+b=c;
+System.out.println("Value of a after swap:"+a);
+System.out.println("Value of a after swap:"+b);
+    }
+}
