14788d70f573d2bb9923e2aa11c7afd30b1ea256
adarshadhunti <adarshadhunti@gmail.com>
2022-11-01 22:45:02 +0530

    test

--- src/test/java/assigment2/students.java
+++ src/test/java/assigment2/students.java
@@ -8,7 +8,9 @@ class Student {
     String phone;
     String address;
     String status;
-    
+    public Student() {
+
+    }
         public Student(String nameinit, String email, String phone,String address,String status ) {
         name = nameinit;
         email = email;
@@ -24,22 +26,32 @@ public class students {
         System.out.println("Input number of students:");
         int n = Integer.parseInt(in.nextLine().trim());
         System.out.println("Input Student String name, String email, String phone,String address,String status");
-        Student stu[] = new Student[3];
+        Student[] stu = new Student[3];
         for (int i = 0; i < n; i++) {
+            stu[i] = new Student();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println("enter student details");
+            System.out.println("enter student name");
             stu[i].name = in.next();
+            System.out.println("enter student email");
             stu[i].email = in.next();
+            System.out.println("enter student phone");
             stu[i].phone = in.next();
+            System.out.println("enter student address");
             stu[i].address = in.next();
+            System.out.println("enter student status");
             stu[i].status = in.next();
-            System.out.println("enter next student details");
+
         }
+        System.out.println("enter which student details to display");
+        int r=in.nextInt();
+
+            System.out.println(stu[r].name);
+            System.out.println(stu[r].email);
+            System.out.println(stu[r].phone);
+            System.out.println(stu[r].address);
+            System.out.println(stu[r].status);
         in.close();
-        for (int i = 0; i < n; i++) {
-            System.out.println(stu[i].name);
-            System.out.println(stu[i].email);
-            System.out.println(stu[i].phone);
-            System.out.println(stu[i].address);
-            System.out.println(stu[i].status);
-        }
     }
 }
